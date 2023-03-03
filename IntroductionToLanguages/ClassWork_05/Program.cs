d59362ca18da249ea1bd0764663086bc55689e33
Artemiy Anurov <aptem64@yandex.ru>
2023-03-03 12:05:40 +0300

    fixes for cw and hw

--- IntroductionToLanguages/ClassWork_05/Program.cs
+++ IntroductionToLanguages/ClassWork_05/Program.cs
@@ -5,31 +5,31 @@ Console.WriteLine("3 - Программа ");
 Console.WriteLine("4 - Программа ");
 int numberOfTask = Prompt("Введите номер задачи");
 switch (numberOfTask)
-            {
-                case 1:
-                Console.Clear();
-                Task_32();
-                break;
+{
+    case 1:
+        Console.Clear();
+        Task_32();
+        break;
 
-                case 2:
-                Console.Clear();
-                Task_33();
-                break;
+    case 2:
+        Console.Clear();
+        Task_33();
+        break;
 
-                case 3:
-                Console.Clear();
-                Task_35();
-                break;
+    case 3:
+        Console.Clear();
+        Task_35();
+        break;
 
-                case 4:
-                Console.Clear();
-                Task_37();
-                break;
+    case 4:
+        Console.Clear();
+        Task_37();
+        break;
 
-                default:
-                Console.WriteLine("Номер задачи введен некорректно.");
-                break;
-            }
+    default:
+        Console.WriteLine("Номер задачи введен некорректно.");
+        break;
+}
 
 static int Prompt(string message)// Метод запроса числа.
 {
@@ -40,7 +40,7 @@ static int Prompt(string message)// Метод запроса числа.
 
 static void Task_32() // Метод для изменения знака на противоположный
 {
-    int [] arr = new int [8];
+    int[] arr = new int[8];
     Random random = new Random();
     for (int index = 0; index < arr.Length; index++)
     {
@@ -57,38 +57,38 @@ static void Task_32() // Метод для изменения знака на п
 
 static void Task_33() // Метод для поиска конкретного значения в массиве
 {
-    
-    int [] arr = {6, 7, 19, 345, 3};
+
+    int[] arr = { 6, 7, 19, 345, 3 };
     int number = Prompt("Введите число для поиска");
 
     int index = 0;
 
-    if(FoundNumberOfArray(arr, number, out index))
+    if (FoundNumberOfArray(arr, number, out index))
     {
         Console.WriteLine($"Элемент {number} найден под индексом {index}.");
     }
     else
     {
         Console.WriteLine($"Элемент {number} не найден.");
-    }   
-    
+    }
+
 }
 
-static bool FoundNumberOfArray(int [] arr, int userNumber, out int index)
+static bool FoundNumberOfArray(int[] arr, int userNumber, out int index)
 {
     for (index = 0; index < arr.Length; index++)
     {
         if (arr[index] == userNumber)
         {
-            return true; 
-        }        
+            return true;
+        }
     }
     return false;
 }
 
 static void Task_35() //Метод для поиска конкретных значений в диапазоне от N до N
 {
-    int [] arr = new int [123];
+    int[] arr = new int[123];
     Random random = new Random();
     int count = 0;
 
@@ -111,22 +111,22 @@ static void Task_35() //Метод для поиска конкретных зн
 
 static void Task_37() //Метод для вывода произведения первых на последние элементов массива о_О
 {
-    int [] arr = {1, 2, 3, 4, 5};
-    int [] arr2;
-    
+    int[] arr = { 1, 2, 3, 4, 5 };
+    int[] arr2;
+
     if (arr.Length % 2 != 0) // проверка на четность и нечетность кол-ва эл. массива
     {
-        arr2 = new int [arr.Length / 2 + 1];
-        arr2 [arr.Length / 2] = arr[arr.Length / 2]; //Та самая причинаа, по которой начинающие программисты не любят массивы 
+        arr2 = new int[arr.Length / 2 + 1];
+        arr2[arr.Length / 2] = arr[arr.Length / 2]; //Та самая причинаа, по которой начинающие программисты не любят массивы 
     }
     else
     {
-        arr2 = new int [arr.Length / 2];
+        arr2 = new int[arr.Length / 2];
     }
 
     for (int i = 0; i < arr.Length / 2; i++)
     {
-        arr2[i] = arr[i] * arr [arr.Length - i -1];
+        arr2[i] = arr[i] * arr[arr.Length - i - 1];
     }
 
     Console.WriteLine(String.Join(" ", arr));
--- IntroductionToLanguages/HomeWork_06/Program.cs
+++ IntroductionToLanguages/HomeWork_06/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+Menu();
+
+void Menu() //Меню выбора задачи.
+{
+    bool flag = false;
+
+    while (flag != true)
+    {
+        Console.Clear();
+        Console.WriteLine("Меню выбора задачи.");
+        Console.WriteLine("Введите цифру для выбора задачи:");
+        Console.WriteLine("1 - Программа для вывода количества чётных чисел в массиве.");
+        Console.WriteLine("2 - Программа для посчета суммы элементов массива, стоящих на нечётных позициях.");
+        Console.WriteLine("3 - Программа для расчета разницы между максимальным и минимальным элементов массива.");
+        Console.WriteLine("0 - Для выхода из программы.");
+        
+        bool access = int.TryParse(Console.ReadLine(),out int result);
+        if (access == true)
+        {
+            switch (result)
+            {
+                case 1:
+                Task_34();
+                break;
+
+                case 2:
+                Task_36();
+                break;
+
+                case 3:
+                Task_38();
+                break;
+
+                case 0:
+                flag = true;
+                break;
+
+                default:
+                Console.Clear();
+                Console.WriteLine("Номер задачи введен некорректно. Повторите попытку.");
+                Console.ReadLine();
+                break;
+            }
+        }
+        else 
+        {
+            Console.Clear();
+            Console.WriteLine("Ошибка ввода. Повторите попытку.");
+            Console.ReadLine();
+        }
+    }
+}
+
+int [] CreateAndFillingArray(int minDimension, int maxDimension, int minValue, int maxValue) //Метод для создания и заполнения массива псевдо-рандомными числами.
+{                                                                                            //minDimension - минимальное число размерности массива для метода Random.Next
+    int [] arr = new int [random.Next(minDimension, maxDimension)];                          //maxDimension - максимальное число размерности массива для метода Random.Next
+                                                                                             //minValue - минимальное значение элемента массива метода Random.Next
+    for (int i = 0; i < arr.Length; i++)                                                     //maxValue - максимальное значение элемента массива  метода Random.Next
+    {
+        arr [i] = random.Next(minValue,maxValue);
+    }
+    return arr;
+}
+
+/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
