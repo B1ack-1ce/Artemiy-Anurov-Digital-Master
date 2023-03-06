d19a14a07ebac76a55e5e942a49306d44ba69a78
Artemiy Anurov <aptem64@yandex.ru>
2023-03-07 01:24:15 +0300

    added a new console for classwork_08

---
+++ IntroductionToLanguages/ClassWork_08/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+Menu();
+
+void Menu() // Меню выбора задач.
+{
+    Console.Clear();
+    Console.WriteLine("1 - Программа для ");
+    Console.WriteLine("2 - Программа для ");
+    Console.WriteLine("3 - Программа для ");
+    int numberOfTask = Prompt("Введите номер задачи");
+    switch (numberOfTask)
+    {
+        case 1:
+            Console.Clear();
+            Task_48();
+            break;
+
+        case 2:
+            Console.Clear();
+            Task_49();
+            break;
+
+        case 3:
+            Console.Clear();
+            Task_51();
+            break;
+
+        default:
+            Console.WriteLine("Номер задачи введен некорректно.");
+            break;
+    }
+}
+
+
+static int Prompt(string message) //Метод для упрощения ввода числа с консоли
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
