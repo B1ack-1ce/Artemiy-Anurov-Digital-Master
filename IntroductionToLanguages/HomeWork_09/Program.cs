93fd21bda3a2a3f23e14ccfc2ed217268f58fa52
Artemiy Anurov <aptem64@yandex.ru>
2023-03-12 12:59:34 +0300

    fixed_02

--- IntroductionToLanguages/HomeWork_09/Program.cs
+++ IntroductionToLanguages/HomeWork_09/Program.cs
@@ -105,11 +105,3 @@ int AckermannFunction (int numberM, int numberN)
     if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
     return AckermannFunction(numberM, numberN);
 }
-
-/*
-Задача 68: 
-Напишите программу вычисления функции Аккермана с помощью рекурсии. 
-Даны два неотрицательных числа m и n.
-m = 2, n = 3 -> A(m,n) = 9
-m = 3, n = 2 -> A(m,n) = 29
-*/
